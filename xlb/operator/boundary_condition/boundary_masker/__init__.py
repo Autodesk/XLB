93a130d4a875c359c356fa526db5d002706edae9
Oliver <loliverhennigh101@gmail.com>
2024-02-20 16:12:01 -0800

    almost done

---
+++ xlb/operator/boundary_condition/boundary_masker/__init__.py
@@ -0,0 +1,2 @@
+from xlb.operator.boundary_condition.boundary_masker.boundary_masker import BoundaryMasker
+from xlb.operator.boundary_condition.boundary_masker.indices_boundary_masker import IndicesBoundaryMasker
