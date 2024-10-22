9f6f9a58f93ca7b29a361c01aae44307065ca064
Mehdi Ataei <mehdi.ataei@autodesk.com>
2024-10-22 00:53:40 -0400

    Added setup for packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="XLB",
-    version="0.0.1",
-    author="",
+    name='xlb',
+    version='0.2.0',
+    description='XLB: Accelerated Lattice Boltzmann (XLB) for Physics-based ML',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Mehdi Ataei',
+    url='https://github.com/Autodesk/XLB',
+    license='Apache License 2.0',
     packages=find_packages(),
-    install_requires=[],
-    include_package_data=True,
+    install_requires=[
+        'jax[cuda]>=0.4.34',
+        'matplotlib>=3.9.2',
+        'numpy>=2.1.2',
+        'pyvista>=0.44.1',
+        'trimesh>=4.4.9',
+        'warp-lang>=1.4.0',
+        'numpy-stl>=3.1.2',
+        'pydantic>=2.9.1',
+        'ruff>=0.6.5'
+    ],
+    python_requires='>=3.10',
 )
