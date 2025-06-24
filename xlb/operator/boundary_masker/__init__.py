0a39df04fbaad7138e804b22e0faf64508937c48
Hesam Salehipour <hesam.salehipour@autodesk.com>
2025-06-24 10:39:58 -0400

    deleted the old multires boundary masker

--- xlb/operator/boundary_masker/__init__.py
+++ xlb/operator/boundary_masker/__init__.py
@@ -6,6 +6,5 @@ from xlb.operator.boundary_masker.ray import MeshMaskerRay
 from xlb.operator.boundary_masker.winding import MeshMaskerWinding
 from xlb.operator.boundary_masker.aabb_fill import MeshMaskerAABBFill
 from xlb.operator.boundary_masker.mesh_voxelization_method import MeshVoxelizationMethod
-from xlb.operator.boundary_masker.multires_boundary_masker import MultiresBoundaryMasker
 from xlb.operator.boundary_masker.multires_aabb import MultiresMeshMaskerAABB
 from xlb.operator.boundary_masker.multires_indices_boundary_masker import MultiresIndicesBoundaryMasker
