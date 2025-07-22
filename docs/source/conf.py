a6de29a76a50e82f7767dc628f55ce1e9b52eb96
Medyan Naser <medyan7.naser@gmail.com>
2025-07-21 21:55:35 -0400

    Add basic functionality to the web page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,12 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "myst_parser",            # for Markdown support
+    "sphinx.ext.autodoc",     # for docstrings
+    "sphinx.ext.napoleon",    # for Google-style/Numpy-style docstrings
+    "sphinx_book_theme",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +29,19 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
 html_static_path = ['_static']
+
+html_theme = "sphinx_book_theme"
+html_theme_options = {
+    "repository_url": "https://github.com/Autodesk/XLB.git",
+    "use_repository_button": True,
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "use_fullscreen_button": True,
+}
+
+extensions.append("myst_parser")
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
