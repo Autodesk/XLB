3da3152cc05885610014344fff4eacaaef4abfe5
Mehdi Ataei <mehdi.ataei@autodesk.com>
2026-01-12 17:41:16 -0500

    Bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="xlb",
-    version="0.3.0",
+    version="0.3.1",
     description="XLB: Accelerated Lattice Boltzmann (XLB) for Physics-based ML",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
